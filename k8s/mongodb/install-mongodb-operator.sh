36a0f1c733500508c051221f9e31e059e8d15c61
Leon <lirufei0808@gmail.com>
2026-06-01 17:40:50 +0800

    update tap-deploy

--- k8s/mongodb/install-mongodb-operator.sh
+++ k8s/mongodb/install-mongodb-operator.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+# Install MongoDB Community Operator
+# Usage: bash install-mongodb-operator.sh [REGISTRY_URL]
+# If REGISTRY_URL is provided, all MongoDB images will be pulled from that registry
+
+REGISTRY_URL="${1:-}"
+
 helm repo add mongodb https://mongodb.github.io/helm-charts
 helm repo update
 
-helm install community-operator mongodb/community-operator \
+HELM_SET_ARGS="--set operator.watchNamespace=\"*\""
+
+if [ -n "$REGISTRY_URL" ]; then
+  echo "Configuring MongoDB operator to use image registry: $REGISTRY_URL"
+  HELM_SET_ARGS="${HELM_SET_ARGS} --set registry.operator=${REGISTRY_URL}"
+  HELM_SET_ARGS="${HELM_SET_ARGS} --set registry.agent=${REGISTRY_URL}"
+  HELM_SET_ARGS="${HELM_SET_ARGS} --set registry.versionUpgradeHook=${REGISTRY_URL}"
+  HELM_SET_ARGS="${HELM_SET_ARGS} --set registry.readinessProbe=${REGISTRY_URL}"
+  HELM_SET_ARGS="${HELM_SET_ARGS} --set mongodb.repo=${REGISTRY_URL}"
+fi
+
+eval "helm install community-operator mongodb/community-operator \
   --namespace mongodb \
   --create-namespace \
-  --set operator.watchNamespace="*"
-
-
+  ${HELM_SET_ARGS}"
